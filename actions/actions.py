ef1505179892786a7ca004819568a713dd296bd9
WendyMwiti <wendymwiti4@gmail.com>
2023-01-16 14:20:33 +0300

    Wrapping up

--- actions/actions.py
+++ actions/actions.py
@@ -3,17 +3,38 @@ from rasa_sdk.events import EventType
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 from typing import Any, Text, Dict, List
+import openai
 
-class ActionDefaultFallback(Action):
-    def init(self):
-        # self.gpt3 = gpt3()
-        super()._init_()
-
-    def name(self) -> Text:
-        return "action_default_fallback"
-
+class GPT3ResponseAction(Action):
+    def __init__(self):
+        super().__init__()
+        openai.api_key =  ""
+        self.model_engine = "text-davinci-003"
+    def name(self):
+        return "action_gpt3_response"
+    # async def run(self, dispatcher, tracker, domain):
+    #     prompt = tracker.latest_message['text']
+    #     response = self.gpt3(prompt)
+    #     dispatcher.utter_message(text=response)
     async def run(self, dispatcher, tracker, domain):
-        query = tracker.latest_message['text']
-        dispatcher.utter_message(text=gpt3(query))
-
-        return []
+        # Get the user's message
+        message = tracker.latest_message['text']
+        # Check if the message contains certain keywords or phrases related to diabetes
+        diabetes_keywords = ["hi", "diabetes", "blood sugar", "insulin", "glucose", "diet", "nutrition", "medicine", "mellitus", "HbA1c", "hypoglycemia", "hyperglycemia", "goodbye"]
+        if any(keyword in message.lower() for keyword in diabetes_keywords):
+            prompt = "question about diabetes: " + message
+            response = self.gpt3(prompt)
+            dispatcher.utter_message(text=response)
+        else:
+            dispatcher.utter_message(text="Sorry, I am not able to answer that question.")
+    def gpt3(self, prompt):
+        completion = openai.Completion.create(
+            engine=self.model_engine,
+            prompt=prompt,
+            max_tokens=1024,
+            n=1,
+            stop="end diabetes",
+            temperature=0.5,
+        )
+        response = completion.choices[0].text
+        return response
